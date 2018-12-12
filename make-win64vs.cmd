a01ab62cf8535d17d352a3175f6381f60b3ab976
Adam Szofran <adam@visualvocal.com>
2018-12-11 16:32:13 -0800

    Build in Visual Studio and exclude image file packages.

--- make-win64vs.cmd
+++ make-win64vs.cmd
@@ -3,7 +3,15 @@ setlocal
 path=%ProgramFiles%\CMake\bin;%ProgramFiles%\NASM;%path%
 if not exist build\win64vs mkdir build\win64vs
 pushd build\win64vs
-cmake -G"Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=install %~dp0
+cmake -G"Visual Studio 15 2017 Win64" ^
+    -DCMAKE_INSTALL_PREFIX=install ^
+    -DCMAKE_DISABLE_FIND_PACKAGE_GIF=TRUE ^
+    -DCMAKE_DISABLE_FIND_PACKAGE_JPEG=TRUE ^
+    -DCMAKE_DISABLE_FIND_PACKAGE_PNG=TRUE ^
+    -DCMAKE_DISABLE_FIND_PACKAGE_TIFF=TRUE ^
+    -DCMAKE_DISABLE_FIND_PACKAGE_ZLIB=TRUE ^
+    -DCMAKE_DISABLE_FIND_PACKAGE_PkgConfig=TRUE ^
+    %~dp0
 popd
 endlocal
 
