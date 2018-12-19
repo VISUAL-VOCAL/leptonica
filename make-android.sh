2dc2ac756f17abbd4a43e95144255006ae9034f0
Adam-VisualVocal <adam@visualvocal.com>
2018-12-19 11:09:15 -0800

    Android build changes.

--- make-android.sh
+++ make-android.sh
@@ -11,41 +11,42 @@ BUILD_DIRECTORY="${SOURCE_DIRECTORY}/${BUILD_DIRECTORY_SUFFIX}"
 
 CONFIGURE_FLAGS="--without-jpeg --without-zlib --without-libpng --without-giflib --without-libtiff --without-libwebp --without-libopenjpeg --disable-programs"
 
-NDK_PATH="${SOURCE_DIRECTORY}/../ndk/android-ndk-r16"
-TOOLCHAIN="${SOURCE_DIRECTORY}/../ndk/toolchains/r16-arm-19"
+NDK_PATH="${SOURCE_DIRECTORY}/../ndk/android-ndk-r18b"
+TOOLCHAIN="${SOURCE_DIRECTORY}/../ndk/toolchains/r18b-arm-24"
 SYSROOT="${TOOLCHAIN}/sysroot"
 BUILD_PLATFORM="linux-x86_64"
 TOOLCHAIN_VERSION="4.9"
-ANDROID_VERSION="19"
+ANDROID_VERSION="24"
 
 # It should not be necessary to modify the rest
 TARGET_HOST=arm-linux-androideabi
-ANDROID_CFLAGS="-march=armv7-a -mfloat-abi=softfp -fprefetch-loop-arrays --sysroot=${SYSROOT} -D__ANDROID_API__=${ANDROID_VERSION}"
+ANDROID_CFLAGS="-march=armv7-a -mfloat-abi=softfp --sysroot=${SYSROOT} -D__ANDROID_API__=${ANDROID_VERSION}"
 
 ANDROID_INCLUDES="-I${SYSROOT}/usr/include -I${TOOLCHAIN}/lib/gcc/${TARGET_HOST}/${TOOLCHAIN_VERSION}.x/include -I${TOOLCHAIN}/prebuilt_include/clang/include"
 
+export AR=${TOOLCHAIN}/bin/${TARGET_HOST}-ar
+export AS=${TOOLCHAIN}/bin/${TARGET_HOST}-clang
 export CC=${TOOLCHAIN}/bin/${TARGET_HOST}-clang
 export CXX=${TOOLCHAIN}/bin/${TARGET_HOST}-clang++
-export AR=${TOOLCHAIN}/bin/${TARGET_HOST}-ar
-export RANLIB=${TOOLCHAIN}/bin/${TARGET_HOST}-ranlib
-export NM=${TOOLCHAIN}/bin/${TARGET_HOST}-nm
-
-export CPP=${TOOLCHAIN}/bin/${TARGET_HOST}-cpp
-export AS=${TOOLCHAIN}/bin/${TARGET_HOST}-clang
 export LD=${TOOLCHAIN}/bin/${TARGET_HOST}-ld
-export OBJDUMP=${TOOLCHAIN}/bin/${TARGET_HOST}-objdump
 export STRIP=${TOOLCHAIN}/bin/${TARGET_HOST}-strip
+
+#export RANLIB=${TOOLCHAIN}/bin/${TARGET_HOST}-ranlib
+#export NM=${TOOLCHAIN}/bin/${TARGET_HOST}-nm
+
+#export CPP=${TOOLCHAIN}/bin/${TARGET_HOST}-cpp
+#export OBJDUMP=${TOOLCHAIN}/bin/${TARGET_HOST}-objdump
 
 export PATH=${PATH}:${TOOLCHAIN}/bin
 export NDK=${NDK_PATH}
 export ANDROID_NDK_ROOT=${NDK_PATH}
 export SYSROOT=${SYSROOT}
 
-export CFLAGS="${INCLUDE_DIRS} ${LIB_DIRS} ${ANDROID_INCLUDES} ${ANDROID_CFLAGS} -O3 -fPIC -pie"
-export CXXFLAGS="${INCLUDE_DIRS} ${LIB_DIRS} ${ANDROID_INCLUDES} ${ANDROID_CFLAGS} -O3 -fPIC -pie"
+export CFLAGS="${INCLUDE_DIRS} ${LIB_DIRS} ${ANDROID_INCLUDES} ${ANDROID_CFLAGS} -O3 -fPIC -fPIE"
+export CXXFLAGS="${INCLUDE_DIRS} ${LIB_DIRS} ${ANDROID_INCLUDES} ${ANDROID_CFLAGS} -O3 -fPIC -fPIE"
 export CPPFLAGS="${INCLUDE_DIRS} ${LIB_DIRS} ${ANDROID_INCLUDES} ${ANDROID_CFLAGS}"
 export ASFLAGS="${INCLUDE_DIRS} ${LIB_DIRS} ${ANDROID_CFLAGS}"
-export LDFLAGS="${INCLUDE_DIRS} ${LIB_DIRS} ${ANDROID_CFLAGS} -fPIC -pie"
+export LDFLAGS="${INCLUDE_DIRS} ${LIB_DIRS} ${ANDROID_CFLAGS} -pie"
 
 cd ${SOURCE_DIRECTORY}
 mkdir --parents ${BUILD_DIRECTORY}
@@ -56,12 +57,12 @@ cd ${BUILD_DIRECTORY}
 sh ${SOURCE_DIRECTORY}/configure \
   --host=${TARGET_HOST} \
   --prefix=${BUILD_DIRECTORY}/install \
-  --disable-nls \
+  ${CONFIGURE_FLAGS} \
   CFLAGS="${CFLAGS}" \
   CPPFLAGS="${CPPFLAGS}" \
   LDFLAGS="${LDFLAGS}" \
   LIBS=-lm \
-  --with-simd ${1+"$@"}
+  ${1+"$@"}
 
 make
 make install-strip
